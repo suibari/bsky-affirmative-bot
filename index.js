198d57f8c6b611c2a725fa30cce9f364e75c5ba1
suibari <404.not.found.address@gmail.com>
2024-03-21 01:49:33 +0900

    例外処理追加

--- index.js
+++ index.js
@@ -14,35 +14,40 @@ const db = new PostgreSQL();
 //     * その人をフォローバック
 //     * その人にあいさつポスト
 async function doFollowAndGreetIfFollowed() {
-  console.log(`[INFO] start follow check.`);
-  const timer = new TimeLogger();
-  timer.tic();
+  try {
+    console.log(`[INFO] start follow check.`);
+    const timer = new TimeLogger();
+    timer.tic();
 
-  await agent.createOrRefleshSession();
-  const notifications = await agent.listUnreadNotifications();
-  for (let notification of notifications) {
-    if (notification.reason == 'follow') {
-      const did = notification.author.did;
-      const isExist = await db.selectDb(did);
-      if (!isExist) {
-        console.log(`[INFO] detect new follower: ${did} !!`);
-        await agent.follow(did);
-        
-        const response = await agent.getAuthorFeed({actor: did, filter: 'posts_no_replies'});
-        const latestFeed = agent.getLatestFeedWithoutMentionAndSpam(notification.author, response.data.feed);
-        if (latestFeed) {
-          await agent.replyGreets(latestFeed.post);
-        }
+    await agent.createOrRefleshSession();
+    const notifications = await agent.listUnreadNotifications();
+    for (let notification of notifications) {
+      if (notification.reason == 'follow') {
+        const did = notification.author.did;
+        const isExist = await db.selectDb(did);
+        if (!isExist) {
+          console.log(`[INFO] detect new follower: ${did} !!`);
+          await agent.follow(did);
+          
+          const response = await agent.getAuthorFeed({actor: did, filter: 'posts_no_replies'});
+          const latestFeed = agent.getLatestFeedWithoutMentionAndSpam(notification.author, response.data.feed);
+          if (latestFeed) {
+            await agent.replyGreets(latestFeed.post);
+          }
 
-        await db.insertDb(did);
+          await db.insertDb(did);
+        };
       };
     };
-  };
-  await agent.updateSeenNotifications(new Date().toISOString());
+    await agent.updateSeenNotifications(new Date().toISOString());
 
-  // db.closeDb();
-  const elapsedTime = timer.tac();
-  console.log(`[INFO] finish follow check, elapsed time is ${elapsedTime} [sec].`);
+    // db.closeDb();
+    const elapsedTime = timer.tac();
+    console.log(`[INFO] finish follow check, elapsed time is ${elapsedTime} [sec].`);
+
+  } catch(e) {
+    console.error(e);
+  }
 }
 setInterval(doFollowAndGreetIfFollowed, 5 * 60 * 1000); // 5 minutes
 // doFollowAndGreetIfFollowed();
@@ -55,33 +60,38 @@ setInterval(doFollowAndGreetIfFollowed, 5 * 60 * 1000); // 5 minutes
 //       - 全肯定をリプライ
 //       - DBの反応時間を更新
 async function doPostAffirmation() {
-  console.log(`[INFO] start post loop.`);
-  const timer = new TimeLogger();
-  timer.tic();
+  try {
+    console.log(`[INFO] start post loop.`);
+    const timer = new TimeLogger();
+    timer.tic();
 
-  await agent.createOrRefleshSession();
-  const followers = await agent.getConcatFollowers(process.env.BSKY_IDENTIFIER, Infinity);
-  for (let follower of followers) {
-    const did = follower.did;
-    const response = await agent.getAuthorFeed({actor: did, filter: 'posts_no_replies'});
-    const feeds = response.data.feed;
-    const latestFeed = agent.getLatestFeedWithoutMentionAndSpam(follower, feeds);
-    if (latestFeed) {
-      const postedAt = new Date(latestFeed.post.indexedAt);
-      const updatedAt = await db.selectDb(did);
-      if ((postedAt > updatedAt) || (!updatedAt)) {
-        console.log(`[INFO] detect new post: ${did} !!`);
-        agent.replyAffermativeWord(latestFeed.post);
+    await agent.createOrRefleshSession();
+    const followers = await agent.getConcatFollowers(process.env.BSKY_IDENTIFIER, Infinity);
+    for (let follower of followers) {
+      const did = follower.did;
+      const response = await agent.getAuthorFeed({actor: did, filter: 'posts_no_replies'});
+      const feeds = response.data.feed;
+      const latestFeed = agent.getLatestFeedWithoutMentionAndSpam(follower, feeds);
+      if (latestFeed) {
+        const postedAt = new Date(latestFeed.post.indexedAt);
+        const updatedAt = await db.selectDb(did);
+        if ((postedAt > updatedAt) || (!updatedAt)) {
+          console.log(`[INFO] detect new post: ${did} !!`);
+          agent.replyAffermativeWord(latestFeed.post);
 
-        db.insertOrUpdateDb(did);
-      } else {
-        console.log(`[INFO] not detect new post: ${did}.`);
+          db.insertOrUpdateDb(did);
+        } else {
+          console.log(`[INFO] not detect new post: ${did}.`);
+        }
       }
     }
+    // db.closeDb();
+    const elapsedTime = timer.tac();
+    console.log(`[INFO] finish post loop, elapsed time is ${elapsedTime} [sec].`);
+
+  } catch(e) {
+    console.error(e);
   }
-  // db.closeDb();
-  const elapsedTime = timer.tac();
-  console.log(`[INFO] finish post loop, elapsed time is ${elapsedTime} [sec].`);
 }
 setInterval(doPostAffirmation, 20 * 60 * 1000); // 20 minutes
 // doPostAffirmation();
