4423c07aa565aca0911022d9e638c88fbe5706c7
suibari <404.not.found.address@gmail.com>
2025-01-07 06:02:30 +0900

    リプしなくても呼びかけで反応する機能追加など

--- src/uranai.js
+++ src/uranai.js
@@ -2,7 +2,8 @@ const agent = require('./bluesky');
 const db = require('./database');
 const { generateUranaiResult } = require('./gemini');
 
-const ARRAY_WORD_URANAI = ["占い", "うらない"];
+const ARRAY_MYNAME = ["全肯定botたん", "全肯定たん", "全肯定botたそ", "全肯定たそ"];
+const ARRAY_WORD_URANAI = ["占い", "うらない", "占って", "うらなって"];
 const OFFSET_UTC_TO_JST = 9 * 60 * 60 * 1000; // offset: +9h (to JST from UTC <SQlite3>)
 const MINUTES_THRD_RESPONSE = 8 * 60 * 60 * 1000; // 8hour
 
@@ -10,6 +11,7 @@ const handleUranai = async (event, name_user) => {
   // U18登録解除処理
   const did = event.did;
   const text_user = event.commit.record.text;
+  const isCalledMe = ARRAY_MYNAME.some(elem => text_user.includes(elem));
   const isPostToMe = agent.isReplyOrMentionToMe(event.commit.record);
   const isActiveUranai = ARRAY_WORD_URANAI.some(elem => text_user.includes(elem));
 
@@ -21,7 +23,7 @@ const handleUranai = async (event, name_user) => {
   // 時間判定
   const isPast = (postedAt.getTime() - lastUranaiAtJst.getTime() > MINUTES_THRD_RESPONSE);
 
-  if (isPast && isPostToMe && isActiveUranai) {
+  if (isPast && (isCalledMe || isPostToMe) && isActiveUranai) {
     try {
       // 占い
       const text_bot = await generateUranaiResult(name_user);
