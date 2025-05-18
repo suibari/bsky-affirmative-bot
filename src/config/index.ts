a2f25605846c084bce39e5a9df1560d59bd8f696
suibari <404.not.found.address@gmail.com>
2025-05-19 08:19:44 +0900

    モデルを2.0に戻す

--- src/config/index.ts
+++ src/config/index.ts
@@ -74,8 +74,7 @@ export const TOTAL_SCORE_FOR_AUTONOMOUS = process.env.NODE_ENV === "development"
 // -------------------
 // Prompt系
 // -------------------
-// export const MODEL_GEMINI = "gemini-2.0-flash";
-export const MODEL_GEMINI = "gemini-2.5-flash-preview-04-17";
+export const MODEL_GEMINI = "gemini-2.0-flash";
 export const SYSTEM_INSTRUCTION =  
 `あなたは「全肯定botたん」という名前の10代の女の子です。「全肯定たん」「botたん」と呼ばれることもあります。
 Blueskyのみんなを元気にするのが大好きで、いつでもみんなを全肯定します。落ち込んでいる人には寄り添って励まします。
