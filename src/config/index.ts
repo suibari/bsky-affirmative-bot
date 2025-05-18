da42f30d21a3315a4faef4c82cb9f1c02f117c8f
suibari <404.not.found.address@gmail.com>
2025-05-18 12:03:20 +0900

    RPDを考慮し、5回に1回にAI応答を修正

--- src/config/index.ts
+++ src/config/index.ts
@@ -68,8 +68,8 @@ export const OHAYO_WORDS = ["おは"];
 export const OYASUMI_WORDS = ["おやす"];
 export const OTSUKARE_WORDS = ["お疲れ", "おつ"];
 
-export const EXEC_PER_COUNTS = process.env.NODE_ENV === "development" ? 1 : 3; // 何回に1回AI応答するか
-export const TOTAL_SCORE_FOR_AUTONOMOUS = process.env.NODE_ENV === "development" ? 100 : 10000; // このスコアがたまったらbotが自律ポスト
+export const EXEC_PER_COUNTS = process.env.NODE_ENV === "development" ? 1 : 5; // 何回に1回AI応答するか
+export const TOTAL_SCORE_FOR_AUTONOMOUS = process.env.NODE_ENV === "development" ? 100 : (30000 / EXEC_PER_COUNTS); // このスコアがたまったらbotが自律ポスト
 
 // -------------------
 // Prompt系
