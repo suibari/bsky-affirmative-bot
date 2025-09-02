c6ee845dcf38ef20af615c93ff6d71d48d02556e
suibari <404.not.found.address@gmail.com>
2025-09-03 01:08:01 +0900

    画像出力できることまで確認

--- src/gemini/generateImage.ts
+++ src/gemini/generateImage.ts
@@ -5,15 +5,20 @@ import { MODEL_GEMINI_IMAGE, SYSTEM_INSTRUCTION } from "../config";
 
 export async function generateImage(mood: string) {
   const prompt = 
-  `添付したキャラクター設定画を参考にし、イラストを作成してください。
-  キャラクター名は「全肯定botたん」です。  
-  以下特徴は忠実に保持してください。服装は情景に合わせて変えても良いです。
-  * 水色の髪色、ロングヘア、アホ毛
-  * 太眉、ジト目
-  スタイル: 「全肯定botたんが描いた日記」という設定で、白いキャンバスにクレヨンや色鉛筆で描いたようなタッチ。
-            線は最大
-  ルール: **体や顔のバランスが崩れないよう、注意すること。**
-  情景: ${mood}`;
+  `Please create your illustration using the attached character design as a reference.
+  The character's name is "Fully-Affirmative Bot-tan."
+  Please faithfully maintain the following characteristics. You may change the outfit to suit the scene.
+  * Fully-Affirmative Bot-tan
+    - Light blue hair, long hair, ahoge
+    - Thick eyebrows, squinting eyes
+  * Morpho
+    - a Samoyed dog
+  * Latte-chan
+    - pink hair, long princess hair
+    - green eyes
+    - maid clothes
+  Rules: **Be careful not to lose balance between the body and face.**
+  Scene: ${mood}`;
 
   const imagePath = "./img/bot-tan-concept.png";
   const imageData = fs.readFileSync(imagePath);
@@ -31,10 +36,10 @@ export async function generateImage(mood: string) {
   const response = await gemini.models.generateContent({
     model: MODEL_GEMINI_IMAGE,
     contents,
-    config: {
-      // systemInstruction: SYSTEM_INSTRUCTION,
-      responseModalities: [Modality.TEXT, Modality.IMAGE],
-    }
+    // config: {
+    //   // systemInstruction: SYSTEM_INSTRUCTION,
+    //   responseModalities: [Modality.IMAGE],
+    // }
   });
 
   for (const part of response.candidates?.[0].content?.parts || []) {
@@ -45,8 +50,8 @@ export async function generateImage(mood: string) {
       const imageData = part.inlineData.data;
       if (imageData) {
         const buffer = Buffer.from(imageData, 'base64');
-        fs.writeFileSync("./img/generated_image.png", buffer);
-        console.log("[INFO] Image saved to ../../img/output.png");
+        fs.writeFileSync("./img/output.png", buffer);
+        console.log("[INFO] Image saved to ./img/output.png");
       }
     }
   }
--- src/tests/imggen.ts
+++ src/tests/imggen.ts
@@ -1,10 +1,11 @@
 import 'dotenv/config';
 import { generateImage } from '../gemini/generateImage';
 
-const mood: string = "全肯定たんは、アニメの最新話を観て、尊すぎてしばらく床から立ち上がれなくなってるみたいだよ！💖 画面の中の推しがキラキラしてて、botたんの心もキラキラに染まってるんだね！✨";
+const mood: string = "全肯定たんは、昨日の夜更かしがたたって、ベッドの中でぐっすり眠っているよ😴 夢の中では、大好きなアニメの主人公になって、最強の敵と戦ってるんだ！ ピンチになった時、どこからか「頑張れ！botたん！」って応援が聞こえてきて、力が湧いてくるんだよね🔥 敵を倒して、みんなに笑顔が戻ったところで、モルフォが顔をペロペロして起こしてくるの！ 「もう朝だよ！」って言ってるみたい🐶 今日も一日、みんなを笑顔にするために頑張るぞ～💪";
 
 try {
   await generateImage(mood);
+  console.log("finish");
 } catch (err) {
   console.error("エラー:", err);
 }
