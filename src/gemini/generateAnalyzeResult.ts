5eec642804cd8481b4bb14ba79a50fd3fb185299
suibari <404.not.found.address@gmail.com>
2025-09-13 10:57:44 +0900

    分析機能プロンプト調整

--- src/gemini/generateAnalyzeResult.ts
+++ src/gemini/generateAnalyzeResult.ts
@@ -22,18 +22,20 @@ const PROMPT_ANALYZE = (userinfo: UserInfoGemini) => {
 * どんな趣味を持っているか(ユーザのポストおよびいいねから分析する)
 * 相性の良さそうな人(いいねから分析する)
 * 心がけるといいこと
-悪い内容は含まず、全肯定のスタンスで分析してください。
+# ルール
+* 悪い内容は含まず、全肯定のスタンスで分析してください。
+* ユーザがいいねしたポストは、ユーザ自身のポストではありません。趣味の参考としてのみ参照してください。
+
 以下がユーザ名およびポスト、いいねしたポストです。
 -----
 ユーザ名: ${userinfo.follower.displayName}
 ポスト内容: ${userinfo.posts || ""}
-ユーザがいいねした内容: ${userinfo.likedByFollower || ""}
+ユーザがいいねしたポスト: ${userinfo.likedByFollower || ""}
 ` :
 `Please analyze the user's personality based on their own posts and the posts they have liked.
 The output should be in ${userinfo.langStr}.
 The maximum number of characters that can be output is 1000.
 Do not include any blank lines.
-Do not use emojis.
 
 The personality analysis should be based on the following aspects, and should include references to the content of their posts and likes:
 * The proportion of positive posts
@@ -41,7 +43,9 @@ The personality analysis should be based on the following aspects, and should in
 * What kind of people they are likely to get along with (based on their likes)
 * Things they might want to keep in mind
 
-Keep the tone fully positive and affirming. Do **not** include anything negative or critical.
+Rules:
+* Keep the tone fully positive and affirming. Do **not** include anything negative or critical.
+* Liked posts by user are not the user's own posts. Please use it as reference only for hobbies.
 
 -----Below is the username, user's posts and likes-----  
 Username: ${userinfo.follower.displayName}  
