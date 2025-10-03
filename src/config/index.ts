e865e6394cfad2ff518bc7a93727c01a979cb8c7
suibari <404.not.found.address@gmail.com>
2025-10-04 08:50:49 +0900

    5回に1回AIリプライに変更

--- src/config/index.ts
+++ src/config/index.ts
@@ -103,7 +103,7 @@ export const OHAYO_WORDS = ["おは"];
 export const OYASUMI_WORDS = ["おやす"];
 export const OTSUKARE_WORDS = ["お疲れ", "おつ", "しごおわ"];
 
-export const EXEC_PER_COUNTS = 10; // 何回に1回AI応答するか
+export const EXEC_PER_COUNTS = 5; // 何回に1回AI応答するか
 export const LIMIT_REQUEST_PER_DAY_GEMINI = 1000;
 export const TOTAL_SCORE_FOR_AUTONOMOUS = process.env.NODE_ENV === "development" ? 100 : (30000 / EXEC_PER_COUNTS); // このスコアがたまったらbotが自律ポスト
 
