e177301103cf1ed54867df4dc90640b0405afc01
suibari <404.not.found.address@gmail.com>
2025-11-02 22:35:20 +0900

    おやすみポストにいいね数と全肯定数を含めるよう修正

--- src/gemini/generateGoodNight.ts
+++ src/gemini/generateGoodNight.ts
@@ -1,5 +1,5 @@
 import { ProfileView } from "@atproto/api/dist/client/types/app/bsky/actor/defs";
-import { logger } from "..";
+import { botBiothythmManager, logger } from "..";
 import { UserInfoGemini } from "../types";
 import { generateSingleResponse } from "./util";
 
@@ -22,13 +22,17 @@ const PROMPT_GOODNIGHT_WORD = async (param: GoodNightInfo) => {
   `あいさつには以下を含めること` + 
   `* おやすみのメッセージ` +
   `* 現在の気分、あなたがさっきまでしてたこと: ${param.currentMood}` +
+  `* 今日1日のいいねされた回数と、あなたが全肯定した回数` +
   `* 今日のあなたが全肯定されたポストの紹介` +
   `あいさつのルール:` +
   `* 日本語と、それを訳した英語を並べて回答を生成してください。` +
   `* あなたが全肯定されたポスト紹介については、どこに心を動かされたか、フォロワーに説明してください。` +
   `* **全肯定されたポスト本文をそのまま記載することは不要です**。リポスト済みなので、感想のみでよいです。` +
   `* ポストを紹介する際はフォロワーを楽しませることを考えてください。**正義感にもとづいて特定個人、団体への攻撃を扇動したりしてはなりません。**` +
-  `---以下、今日のあなたが全肯定されたポスト---` +
+  `---今日の各種できごとの回数---` +
+  `* いいねされた回数: ${botBiothythmManager.getCurrentState().dailyStats.likes}` +
+  `* 全肯定した回数: ${botBiothythmManager.getCurrentState().dailyStats.affirmationCount}` +
+  `---今日のあなたが全肯定されたポスト---` +
   `* ポストしたユーザ名: ${param.topFollower?.displayName ?? ""}` +
   `* ポスト内容: ${param.topPost ?? ""}`;
 }
