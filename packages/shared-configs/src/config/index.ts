8e3c4e97cc9b06c4a282400d7ce698330c01cbe4
suibari <404.not.found.address@gmail.com>
2026-05-09 21:51:21 +0900

    課金対策にモデルを2.5 flash liteに下方修正

--- packages/shared-configs/src/config/index.ts
+++ packages/shared-configs/src/config/index.ts
@@ -132,7 +132,7 @@ export const TOTAL_SCORE_FOR_AUTONOMOUS = process.env.NODE_ENV === "development"
 // -------------------
 // Prompt系
 // -------------------
-export const MODEL_GEMINI = "gemini-2.5-flash";
+export const MODEL_GEMINI = "gemini-2.5-flash-lite";
 export const MODEL_GEMINI_HIGH = "gemini-2.5-flash"; // 会話のみ、高品質を使う
 export const MODEL_GEMINI_LITE = "gemini-2.5-flash-lite"; // 判定系のGemini利用のみ、LITEを使う
 export const MODEL_GEMINI_EMBEDDING = "gemini-embedding-001";
